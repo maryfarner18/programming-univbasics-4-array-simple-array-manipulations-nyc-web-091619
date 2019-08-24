523f599fa659d5770ddd81414ae2f195edae64c3
Mary Farner <maryfarner@gmail.com>
2019-08-24 14:29:48 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,41 @@
+def using_push(arr, stg)
+  arr.push(stg)
+end
+
+def using_unshift(arr, stg)
+  arr.unshift(stg)
+end
+
+def using_pop(arr)
+  arr.pop()
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift()
+end
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
+
+def using_concat
+end
+
+def using_insert
+end
+
+def using_uniq
+end
+
+def using_flatten
+end
+
+def using_delete
+end
+
+def using_delete_at
+end
